d8fea01297fa7d2468ee6430ea476c139de9628b
Kevin Norris <kevin.norris.90@gmail.com>
2017-04-25 09:40:36 +1200

    Add app colors file

---
+++ app/utils/colors.js
@@ -0,0 +1,12 @@
+export const primaryLight = '#f4f1ea';
+export const primary = '#eae7db';
+export const primaryDark = '#907164';
+// Used for nav hover highlight, titles, nav color when not highlighted
+export const primaryDarker = '#382110';
+export const darkerHover = '#58371F';
+
+export const actionColor = '#409D69';
+
+export const linkColor = '#00635d';
+
+export const detailText = '#999999';
