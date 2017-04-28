e182746907836b5750ebac90b9855f7e44ab9735
Kevin Norris <kevin.norris.90@gmail.com>
2017-04-29 08:31:33 +1200

    Add styles and propvalidation to AddBookPage

--- app/containers/AddBookPage/index.js
+++ app/containers/AddBookPage/index.js
@@ -2,8 +2,11 @@ import React, { PropTypes } from 'react';
 import { connect } from 'react-redux';
 import Helmet from 'react-helmet';
 import { createStructuredSelector } from 'reselect';
+import { Row, Col } from 'react-bootstrap';
 
 import Header from 'containers/Header';
+import Title from 'components/Title';
+import Card from 'components/Card';
 import makeSelectAddBookPage from './selectors';
 import Search from './Search';
 import { searchRequest } from './actions';
@@ -19,8 +22,20 @@ export class AddBookPage extends React.PureComponent { // eslint-disable-line re
           ]}
         />
         <Header location={this.props.location.pathname} />
-        <div className="container">
-          <Search search={this.props.searchRequest} />
+        <div className="container text-center">
+          <Title>Search for Your Book</Title>
+          <Row>
+            <Col xs={12} xsOffset={0} sm={6} smOffset={3} md={4} mdOffset={4} >
+              <Search search={this.props.searchRequest} />
+            </Col>
+          </Row>
+          {this.props.AddBookPage.books ?
+            <Card>
+              <Row>
+              </Row>
+            </Card> :
+            null
+          }
         </div>
       </div>
     );
@@ -30,6 +45,12 @@ export class AddBookPage extends React.PureComponent { // eslint-disable-line re
 AddBookPage.propTypes = {
   location: PropTypes.object,
   searchRequest: PropTypes.func.isRequired,
+  AddBookPage: PropTypes.shape({
+    searching: PropTypes.oneOfType([PropTypes.bool, PropTypes.string]),
+    error: PropTypes.oneOfType([PropTypes.bool, PropTypes.string]),
+    books: PropTypes.oneOfType([PropTypes.bool, PropTypes.object]),
+    selectedBook: PropTypes.oneOfType([PropTypes.bool, PropTypes.number]),
+  }),
 };
 
 const mapStateToProps = createStructuredSelector({
